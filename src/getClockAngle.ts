0f235b212d8a3dd82d9e834d540c83cb2510e3f9
chaiyodcymg <chaiyod.cymg@gmail.com>
2024-02-25 01:05:48 +0700

    first commit

---
+++ src/getClockAngle.ts
@@ -0,0 +1,16 @@
+
+export const getClockAngle = (hh_mm: string): number => {
+    const [ hour , minute ]: string [] = hh_mm.split(":")
+    let hourNumber: number = parseInt(hour)
+    hourNumber = hourNumber > 12 ? hourNumber - 12 : hourNumber
+    const minuteNumber: number = parseInt(minute) 
+    const minuteNumberFordivide: number = minuteNumber == 0 ? 60 : minuteNumber
+    const divide: number = minuteNumberFordivide / 60  == 1 ? 0 : minuteNumberFordivide / 60 
+    let hourhand: number = (hourNumber + divide) * 360 / 12
+    hourhand = hourhand >= 360 ? hourhand - 360 : hourhand
+    const minutehand: number = minuteNumber == 0 ? ((60 * 12) / 60) * 360 / 12 : ((minuteNumber * 12) / 60) * 360 / 12 + 360
+    const result: number =  hourhand > minutehand ? hourhand - minutehand : minutehand - hourhand 
+    return result > 180 ? Math.abs(360 - result) : result
+}
+ console.log(getClockAngle("00:00"));
+ 
---
+++ test/getClockAngle.test.ts
@@ -0,0 +1,44 @@
+
+import { getClockAngle } from "../src/getClockAngle"
+
+
+describe("getClockAngle function", () => {
+
+    type Case = {
+        hh_mm: string;
+        expected: number;
+    }
+
+    const cases : Case[] = [
+        {
+            hh_mm: "09:00",
+            expected: 90
+        },
+        {
+            hh_mm: "17:30",
+            expected: 15
+        },
+        {
+            hh_mm: "12:00",
+            expected: 0
+        },
+        {
+            hh_mm: "18:00",
+            expected: 180
+        },
+        {
+            hh_mm: "20:15",
+            expected: 157.5
+        },
+        {
+            hh_mm: "00:00",
+            expected: 0
+        }
+    ];
+
+    test.each(cases)(`getClockAngle(%s) should be %s`, ({ hh_mm , expected}) => {
+        const result = getClockAngle(hh_mm)
+        expect(result).toEqual(expected);
+    })
+
+});
