d02cbd465e9554eb4549d3df7358d0bad0bfc15d
chaiyodcymg <chaiyod.cymg@gmail.com>
2024-03-12 17:20:02 +0700

    Fix bug and add test cases

--- test/quickestPath.test.ts
+++ test/quickestPath.test.ts
@@ -6,8 +6,8 @@ describe("quickestPath function", () => {
     const snakesDefault:  [number, number][] = [ [21, 4], [30, 8],  [55, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
     const laddersNotSort: [number, number][] = [ [47, 86], [14, 35],[3, 39], [31, 70], [71, 93], [44, 65],  [63, 83] ];
     const snakesNotSort:  [number, number][] = [ [30, 8], [96, 66], [55, 38], [79, 42], [91, 48], [21, 4],  [87, 54] ];
-    const laddersOtherBoards: [number, number][] = [ [12, 39], [14, 35], [31, 70], [46, 65], [49, 86], [63, 83], [71, 98] ];
-    const snakesOtherBoards:  [number, number][] = [ [21, 4], [30, 8],  [45, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
+    const laddersOtherBoard: [number, number][] = [ [12, 39], [14, 35], [31, 70], [46, 65], [49, 86], [63, 83], [71, 98] ];
+    const snakesOtherBoard:  [number, number][] = [ [21, 4], [30, 8],  [45, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
 
     const casesDefault : number[][] = [
         [ 2, 5, 6, 5, 2],
@@ -17,7 +17,7 @@ describe("quickestPath function", () => {
         [ 2, 5, 6, 4, 3]
     ];
 
-    const casesOtherBoards : number[][] = [
+    const casesOtherBoard : number[][] = [
         [ 6, 5, 5, 2, 6, 2 ],
         [ 6, 5, 2, 5, 6, 2 ],
         [ 6, 5, 4, 3, 6, 2 ],
@@ -41,8 +41,8 @@ describe("quickestPath function", () => {
     })
 
     test(`quickestPath should be matched cases ladders and snakes are other boards`, () => {
-        const result: number[] = quickestPath({ladders: laddersOtherBoards , snakes: snakesOtherBoards})
-        const findResult = casesOtherBoards.some((value) => JSON.stringify(value) == JSON.stringify(result))
+        const result: number[] = quickestPath({ladders: laddersOtherBoard , snakes: snakesOtherBoard})
+        const findResult = casesOtherBoard.some((value) => JSON.stringify(value) == JSON.stringify(result))
         expect(findResult).toEqual(true)
     })
 });
