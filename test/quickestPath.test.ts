06abcaac2858f6f239e8bdff1e65f6e290eccd1e
chaiyodcymg <chaiyod.cymg@gmail.com>
2024-03-12 17:20:02 +0700

    Fix bug and add test cases

--- test/quickestPath.test.ts
+++ test/quickestPath.test.ts
@@ -2,21 +2,47 @@ import { quickestPath } from "../src/quickestPath"
 
 describe("quickestPath function", () => {
 
-    const ladders: [number, number][] = [ [3, 39], [14, 35], [31, 70], [44, 65], [47, 86], [63, 83], [71, 93] ];
-    const snakes:  [number, number][] = [ [21, 4], [30, 8],  [55, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
-    const cases : number[][] = [
+    const laddersDefault: [number, number][] = [ [3, 39], [14, 35], [31, 70], [44, 65], [47, 86], [63, 83], [71, 93] ];
+    const snakesDefault:  [number, number][] = [ [21, 4], [30, 8],  [55, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
+    const laddersNotSort: [number, number][] = [ [47, 86], [14, 35],[3, 39], [31, 70], [71, 93], [44, 65],  [63, 83] ];
+    const snakesNotSort:  [number, number][] = [ [30, 8], [96, 66], [55, 38], [79, 42], [91, 48], [21, 4],  [87, 54] ];
+    const laddersOtherBoard: [number, number][] = [ [12, 39], [14, 35], [31, 70], [46, 65], [49, 86], [63, 83], [71, 98] ];
+    const snakesOtherBoard:  [number, number][] = [ [21, 4], [30, 8],  [45, 38], [79, 42], [87, 54], [91, 48], [96, 66] ];
+
+    const casesDefault : number[][] = [
         [ 2, 5, 6, 5, 2],
         [ 2, 5, 6, 2, 5],
         [ 2, 5, 6, 6, 1],
         [ 2, 5, 6, 1, 6], 
-        [ 2, 5, 6, 3, 4], 
         [ 2, 5, 6, 4, 3]
     ];
 
-    test(`quickestPath should be matched cases`, () => {
-        const result: number[] = quickestPath({ladders , snakes})
-        const findResult = cases.some((value) => JSON.stringify(value) == JSON.stringify(result))
+    const casesOtherBoard : number[][] = [
+        [ 6, 5, 5, 2, 6, 2 ],
+        [ 6, 5, 2, 5, 6, 2 ],
+        [ 6, 5, 4, 3, 6, 2 ],
+        [ 6, 5, 3, 4, 6, 2 ],
+        [ 5, 6, 5, 2, 6, 2 ],
+        [ 5, 6, 2, 5, 6, 2 ],
+        [ 5, 6, 4, 3, 6, 2 ],
+        [ 5, 6, 3, 4, 6, 2 ],     
+    ];
+
+    test(`quickestPath should be matched cases ladders and snakes are default`, () => {
+        const result: number[] = quickestPath({ladders: laddersDefault , snakes: snakesDefault})
+        const findResult = casesDefault.some((value) => JSON.stringify(value) == JSON.stringify(result))
         expect(findResult).toEqual(true)
     })
 
+    test(`quickestPath should be matched cases ladders and snakes are not sort`, () => {
+        const result: number[] = quickestPath({ladders: laddersNotSort , snakes: snakesNotSort})
+        const findResult = casesDefault.some((value) => JSON.stringify(value) == JSON.stringify(result))
+        expect(findResult).toEqual(true)
+    })
+
+    test(`quickestPath should be matched cases ladders and snakes are other boards`, () => {
+        const result: number[] = quickestPath({ladders: laddersOtherBoard , snakes: snakesOtherBoard})
+        const findResult = casesOtherBoard.some((value) => JSON.stringify(value) == JSON.stringify(result))
+        expect(findResult).toEqual(true)
+    })
 });
